7b2a1d08086f77a357bea1f364418734d59fa518
zacksleo <zacksleo@gmail.com>
2017-03-03 12:36:03 +0800

    style:

---
+++ src/Module.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace zacksleo\yii2\shop;
+
+use yii;
+use yii\base\Module as BaseModule;
+
+/**
+ * portal module definition class
+ */
+class Module extends BaseModule
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'zacksleo\yii2\shop\controllers';
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * Registers the translation files
+     */
+    protected function registerTranslations()
+    {
+        Yii::$app->i18n->translations['zacksleo/yii2/shop/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@zacksleo/yii2/shop/messages',
+            'fileMap' => [
+                'zacksleo/yii2/shop/shop' => 'shop.php',
+            ],
+        ];
+    }
+
+    /**
+     * Translates a message. This is just a wrapper of Yii::t
+     *
+     * @see Yii::t
+     *
+     * @param $category
+     * @param $message
+     * @param array $params
+     * @param null $language
+     * @return string
+     */
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('zacksleo/yii2/shop/' . $category, $message, $params, $language);
+    }
+
+}
