adeed64513e8d0bfc5b5b44fd30c05dca48599b6
zacksleo <zacksleo@gmail.com>
2017-08-30 23:36:06 +0800

    更换cart组件

--- src/models/Item.php
+++ src/models/Item.php
@@ -2,21 +2,19 @@
 
 namespace zacksleo\yii2\shop\models;
 
-use yii2mod\cart\models\CartItemInterface;
+use yz\shoppingcart\CartPositionInterface;
+use yz\shoppingcart\CartPositionTrait;
 
-class Item extends \zacksleo\yii2\cms\models\Item implements CartItemInterface
+class Item extends \zacksleo\yii2\cms\models\Item implements CartPositionInterface
 {
+    use CartPositionTrait;
+
     public function getPrice()
     {
         return $this->price;
     }
 
-    public function getLabel()
-    {
-        return $this->item_name;
-    }
-
-    public function getUniqueId()
+    public function getId()
     {
         return $this->id;
     }
