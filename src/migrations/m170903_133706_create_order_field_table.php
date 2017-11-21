d544c24c1fd6ce6a1ee45cef023a4c27f319377c
zacksleo <zacksleo@gmail.com>
2017-11-21 14:58:32 +0800

    fix(migrations): 订单信息表，编码问题

--- src/migrations/m170903_133706_create_order_field_table.php
+++ src/migrations/m170903_133706_create_order_field_table.php
@@ -12,13 +12,17 @@ class m170903_133706_create_order_field_table extends Migration
      */
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%order_field}}', [
             'id' => $this->primaryKey(),
             'order_id' => $this->integer()->notNull(),
             'type' => $this->string()->notNull(),
             'key' => $this->string()->notNull(),
             'value' => $this->text(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
